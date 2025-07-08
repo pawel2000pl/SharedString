9f5e4106b613fcb51aee6bc6369c07d14d340778
Paweł Bielecki <pawelek24@op.pl>
2025-07-08 07:41:25 +0200

    Added switching to std::string in tests to validate tests

--- src/test_find.cpp
+++ src/test_find.cpp
@@ -1,57 +1,61 @@
 #include <gtest/gtest.h>
+#include <string>
 #include "SharedString.h"
+
+// easy switch to std::string
+using TestString = SharedString<char>;
 
 
 TEST(Find, Basic) {
 
-    SharedString str1("abc##abc##abc");
+    TestString str1("abc##abc##abc");
 
     EXPECT_EQ(str1.find("abc", 0), 0);
     EXPECT_EQ(str1.find("abc", 1), 5);
     EXPECT_EQ(str1.find("abc", 5), 5);
     EXPECT_EQ(str1.find("abc", 6), 10);
-    EXPECT_EQ(str1.find("abc", 11), SharedString<char>::npos);
+    EXPECT_EQ(str1.find("abc", 11), TestString::npos);
 
 }
 
 
 TEST(Find, Overload) {
 
-    SharedString str1("abc##abc##abc");
+    TestString str1("abc##abc##abc");
 
-    EXPECT_EQ(str1.find("abc", 200), SharedString<char>::npos);
-    EXPECT_EQ(str1.find("nope", 0), SharedString<char>::npos);
+    EXPECT_EQ(str1.find("abc", 200), TestString::npos);
+    EXPECT_EQ(str1.find("nope", 0), TestString::npos);
 
 }
 
 
 TEST(Find, EmptyNeedle) {
     
-    SharedString str1("abc##abc##abc");
+    TestString str1("abc##abc##abc");
 
     EXPECT_EQ(str1.find("", 0), 0);
     EXPECT_EQ(str1.find("", 1), 1);
-    EXPECT_EQ(str1.find("", 200), SharedString<char>::npos);
+    EXPECT_EQ(str1.find("", 200), TestString::npos);
 
 }
 
 
 TEST(Find, EmptyHaystack) {
     
-    SharedString str1("");
+    TestString str1("");
 
-    EXPECT_EQ(str1.find("abc", 0), SharedString<char>::npos);
-    EXPECT_EQ(str1.find("", 1), SharedString<char>::npos);
+    EXPECT_EQ(str1.find("abc", 0), TestString::npos);
+    EXPECT_EQ(str1.find("", 1), TestString::npos);
     EXPECT_EQ(str1.find("", 0), 0);
-    EXPECT_EQ(str1.find("", 200), SharedString<char>::npos);
-    EXPECT_EQ(str1.find("abc", 200), SharedString<char>::npos);
+    EXPECT_EQ(str1.find("", 200), TestString::npos);
+    EXPECT_EQ(str1.find("abc", 200), TestString::npos);
 
 }
 
 
 TEST(RFind, Basic) {
 
-    SharedString str1("abc##abc##abc");
+    TestString str1("abc##abc##abc");
 
     EXPECT_EQ(str1.rfind("abc"), 10);
     EXPECT_EQ(str1.rfind("abc", 12), 10);
@@ -65,17 +69,17 @@ TEST(RFind, Basic) {
 
 TEST(RFind, Overload) {
 
-    SharedString str1("abc##abc##abc");
+    TestString str1("abc##abc##abc");
 
     EXPECT_EQ(str1.rfind("abc", 200), 10);
-    EXPECT_EQ(str1.rfind("nope", 0), SharedString<char>::npos);
+    EXPECT_EQ(str1.rfind("nope", 0), TestString::npos);
 
 }
 
 
 TEST(RFind, EmptyNeedle) {
     
-    SharedString str1("abc##abc##abc");
+    TestString str1("abc##abc##abc");
 
     EXPECT_EQ(str1.rfind(""), str1.size());
     EXPECT_EQ(str1.rfind("", 0), 0);
@@ -87,12 +91,12 @@ TEST(RFind, EmptyNeedle) {
 
 TEST(RFind, EmptyHaystack) {
     
-    SharedString str1("");
+    TestString str1("");
 
-    EXPECT_EQ(str1.rfind("abc", 0), SharedString<char>::npos);
+    EXPECT_EQ(str1.rfind("abc", 0), TestString::npos);
     EXPECT_EQ(str1.rfind("", 1), 0);
     EXPECT_EQ(str1.rfind("", 0), 0);
     EXPECT_EQ(str1.rfind("", 200), 0);
-    EXPECT_EQ(str1.rfind("abc", 200), SharedString<char>::npos);
+    EXPECT_EQ(str1.rfind("abc", 200), TestString::npos);
 
 }
